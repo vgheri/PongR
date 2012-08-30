5d5af72eb04f3c647eee8ca6a0338876bc4e98d5
Valerio Gheri <valerio.gheri@gmail.com>
2012-08-30 11:41:49 +0200

    Fixes to the ViewModels
    Fixed the way the ViewModels are built and added some support function

--- PongR/Js/viewModel.js
+++ PongR/Js/viewModel.js
@@ -6,6 +6,16 @@
 
 var pongR = (function (myPongR, $) {
 
+    function getBallX(radius) {
+        var x = $(".ball")[0].style.left;
+        return x - radius;
+    }
+
+    function getBallY(radius) {
+        var y = $(".ball")[0].style.top;
+        return y - radius;
+    }
+
     // ViewModels
     myPongR.Point = function (x, y) {
         var self = this;
@@ -17,30 +27,30 @@ var pongR = (function (myPongR, $) {
         var self = this;
         self.username = username;
         self.connectionId = connectionId;
-    }
+    };
 
     myPongR.Player = function (user) {
         var self = this;
-        self.user = user;
+        self.user = new myPongR.User(user.Username, user.Id);
         self.barDirection = ""; // Can be up or down
         self.barMarginTop = 600; // %
         self.vertexes = []; // TODO getInitialPosition()
         self.score = 0;
     };
 
-    myPongR.Ball = function (x, y, direction) {
+    myPongR.Ball = function (direction) {
         var self = this;
-        self.x = x;
-        self.y = y;
+        self.radius = ($(".plot")[0].offsetWidth) / 2;
+        self.Coordinates = new myPongR.Point(getBallX(radius), getBallY(radius)); 
         self.direction = direction; // can be left or right
     };
 
-    myPongR.App = function (id, player1, player2, ball) {
+    myPongR.App = function (id, player1, player2, ballDirection) {
         var self = this;
         self.playRoomId = id;
         self.player1 = player1;
         self.player2 = player2;
-        self.ball = ball;
+        self.ball = new myPongR.Ball(ballDirection);
         self.field = []; // TODO getFieldVertexes()
     };
 
