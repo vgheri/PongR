9cf530ab0e1d3d8b752bed10ed19dc178ee78685
Valerio Gheri <valerio.gheri@gmail.com>
2012-10-09 16:39:13 +0200

    Removed useless "self" variables
    Replaced with "this" and replaced old myPongR references

--- PongR/Js/PongR.js
+++ PongR/Js/PongR.js
@@ -7,83 +7,74 @@
 // Module creation
 var PongR = (function ($) {
 
-    function PongR(width, height) {
-        var self = this;
-        var viewPort = new self.ViewPort(width, height);
-        self.settings = new self.Settings(viewPort);
+    function PongR(width, height) {        
+        var viewPort = new this.ViewPort(width, height);
+        this.settings = new this.Settings(viewPort);
     }
 
     // ViewModels
-    PongR.prototype.Point = function (x, y) {
-        var self = this;
-        self.x = x;
-        self.y = y;
+    PongR.prototype.Point = function (x, y) {        
+        this.x = x;
+        this.y = y;
     };
 
-    PongR.prototype.ViewPort = function (width, height) {
-        var self = this;
-        self.width = width;
-        self.height = height;
+    PongR.prototype.ViewPort = function (width, height) {        
+        this.width = width;
+        this.height = height;
     };
 
-    PongR.prototype.User = function (username, connectionId) {
-        var self = this;
-        self.username = ko.observable(username);
-        self.connectionId = connectionId;
+    PongR.prototype.User = function (username, connectionId) {        
+        this.username = ko.observable(username);
+        this.connectionId = connectionId;
     };
 
-    PongR.prototype.Input = function (commands, sequenceNumber) {
-        var self = this;
-        self.commands = commands;
-        self.sequenceNumber = sequenceNumber;
+    PongR.prototype.Input = function (commands, sequenceNumber) {        
+        this.commands = commands;
+        this.sequenceNumber = sequenceNumber;
     };
 
-    PongR.prototype.Player = function (user, playerNumber, fieldWidth) {
-        var self = this;
-        self.user = new myPongR.User(user.Username, user.Id);
-        self.playerNumber = playerNumber;
-        self.barWidth = 30;
-        self.barHeight = 96;
-        self.topLeftVertex = null;
+    PongR.prototype.Player = function (user, playerNumber, fieldWidth) {        
+        this.user = new myPongR.User(user.Username, user.Id);
+        this.playerNumber = playerNumber;
+        this.barWidth = 30;
+        this.barHeight = 96;
+        this.topLeftVertex = null;
         if (playerNumber === 1) {
-            self.topLeftVertex = new myPongR.Point(50, 252);
+            this.topLeftVertex = new this.Point(50, 252);
         }
         else {
-            self.topLeftVertex = new myPongR.Point(fieldWidth - 50 - self.barWidth, 252);
+            this.topLeftVertex = new this.Point(fieldWidth - 50 - this.barWidth, 252);
         }
-        self.barDirection = ""; // Can be empty (i.e. not moving), up or down
-        self.inputs = []; // Local history of inputs for this client. Each input is of type myPongR.Input
-        self.score = ko.observable(0);
-        self.lastProcessedInputId = -1;
+        this.barDirection = ""; // Can be empty (i.e. not moving), up or down
+        this.inputs = []; // Local history of inputs for this client. Each input is of type myPongR.Input
+        this.score = ko.observable(0);
+        this.lastProcessedInputId = -1;
     };
 
-    PongR.prototype.Ball = function (direction, fieldWidth, fieldHeight) {
-        var self = this;
-        self.radius = 20;
-        self.position = new myPongR.Point(fieldWidth / 2, fieldHeight / 2); // The ball starts at the center of the field
-        self.direction = direction; // can be left or right        
-        self.angle = (direction === "right" ? 0 : 180);
+    PongR.prototype.Ball = function (direction, fieldWidth, fieldHeight) {        
+        this.radius = 20;
+        this.position = new this.Point(fieldWidth / 2, fieldHeight / 2); // The ball starts at the center of the field
+        this.direction = direction; // can be left or right        
+        this.angle = (direction === "right" ? 0 : 180);
     };
 
-    PongR.prototype.Settings = function (viewPort) {
-        var self = this;
-        self.viewPort = viewPort; // The viewport size as passed by the client
-        self.naive_approach = true; // default : true. Means we won't use lag compensation
-        self.client_prediction = true;
-        self.input_sequence = 0; //When predicting client inputs, we store the last input as a sequence number        
-        self.client_smoothing = false;  //Whether or not the client side prediction tries to smooth things out
-        self.client_smooth = 25;        //amount of smoothing to apply to client update dest
-        self.gap = 30; // px. Minimum distance between the player and the field delimiters (up and down)
-        self.BAR_SCROLL_UNIT = 5; // px
-        self.BALL_FIXED_STEP = 10; // px is the fixed distance that the ball moves (both over x and y axis) between 2 frames
+    PongR.prototype.Settings = function (viewPort) {        
+        this.viewPort = viewPort; // The viewport size as passed by the client
+        this.naive_approach = true; // default : true. Means we won't use lag compensation
+        this.client_prediction = true;
+        this.input_sequence = 0; //When predicting client inputs, we store the last input as a sequence number        
+        this.client_smoothing = false;  //Whether or not the client side prediction tries to smooth things out
+        this.client_smooth = 25;        //amount of smoothing to apply to client update dest
+        this.gap = 30; // px. Minimum distance between the player and the field delimiters (up and down)
+        this.BAR_SCROLL_UNIT = 5; // px
+        this.BALL_FIXED_STEP = 10; // px is the fixed distance that the ball moves (both over x and y axis) between 2 frames
     };
 
-    PongR.prototype.Game = function (id, player1, player2, ballDirection) {
-        var self = this; // Instance of the game        
-        self.gameId = id;
-        self.player1 = new myPongR.Player(player1, 1);
-        self.player2 = new myPongR.Player(player2, 2);        
-        self.ball = new myPongR.Ball(ballDirection, self.fieldWidth, self.fieldHeight);
+    PongR.prototype.Game = function (id, player1, player2, ballDirection) {        
+        this.gameId = id;
+        this.player1 = new this.Player(player1, 1);
+        this.player2 = new this.Player(player2, 2);        
+        this.ball = new this.Ball(ballDirection, this.fieldWidth, this.fieldHeight);
     };
 
     return PongR;
